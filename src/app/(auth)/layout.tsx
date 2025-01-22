1a3e46082938fe8c28f0fce345f44da947f69f4c
tigeryash <yashwanth.venkatesan@gmail.com>
2025-01-22 13:21:39 -0500

    fixed layout issues

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex flex-col items-center justify-center min-h-dvh">
+    <main className="flex flex-col items-center justify-center ">
       <div className="flex flex-col items-center justify-center border rounded-xl py-14 px-10 mt-[5rem] md:mt-[0] w-[90%] ">
         {children}
       </div>
--- src/app/(auth)/register/survey/page.tsx
+++ src/app/(auth)/register/survey/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 
@@ -6,9 +6,24 @@ const FirstSurvey = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
 
   const questions = [
-    { id: 1, question: "What is your weight?", inputType: "number", placeholder: "Enter your weight in kg" },
-    { id: 2, question: "What is your height?", inputType: "number", placeholder: "Enter your height in cm" },
-    { id: 3, question: "Do you have any dietary restrictions?", inputType: "text", placeholder: "e.g., Vegan, Gluten-Free" },
+    {
+      id: 1,
+      question: "What is your weight?",
+      inputType: "number",
+      placeholder: "Enter your weight in kg",
+    },
+    {
+      id: 2,
+      question: "What is your height?",
+      inputType: "number",
+      placeholder: "Enter your height in cm",
+    },
+    {
+      id: 3,
+      question: "Do you have any dietary restrictions?",
+      inputType: "text",
+      placeholder: "e.g., Vegan, Gluten-Free",
+    },
   ];
 
   const nextQuestion = () => {
@@ -18,12 +33,16 @@ const FirstSurvey = () => {
   };
 
   return (
-    <div className="relative h-screen w-full overflow-hidden flex items-center justify-center">
+    <div className="relative h-[calc(100vh-20rem)] w-full overflow-hidden flex items-center justify-center">
       {questions.map((q, index) => (
         <div
           key={q.id}
           className={`absolute top-0 left-0 h-full w-full flex items-center justify-center transform transition-transform duration-500 ${
-            index === currentQuestion ? "translate-x-0" : index < currentQuestion ? "-translate-x-full" : "translate-x-full"
+            index === currentQuestion
+              ? "translate-x-0"
+              : index < currentQuestion
+              ? "-translate-x-full"
+              : "translate-x-full"
           }`}
         >
           <div className="bg-white shadow-lg p-6 rounded-md w-11/12 max-w-md">
