85bd0448829686c6c64a828649b44e94963dd65f
tigeryash <yashwanth.venkatesan@gmail.com>
2025-01-23 14:01:02 -0500

    removed margin-top from auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,14 +2,11 @@ import Navbar from "@/components/navigation/NavBar";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
-      <Navbar />
-      <main className="flex flex-col items-center justify-center min-h-dvh">
-        <div className="flex flex-col items-center justify-center border rounded-xl py-14 px-10 mt-[5rem] md:mt-[0] w-[90%] ">
-          {children}
-        </div>
-      </main>
-    </>
+    <main className="flex flex-col items-center justify-center ">
+      <div className="flex flex-col items-center justify-center border rounded-xl py-14 px-10 md:mt-[0] w-[90%] ">
+        {children}
+      </div>
+    </main>
   );
 };
 
