4c820c48fab9f0f8f16b915db4396e3da07bd31d
Yash <yashwanth.venkatesan@gmail.com>
2025-01-29 18:48:44 -0500

    implemented design for settings page

---
+++ src/app/(dashboard)/profile/settings/components/display-settings.tsx
@@ -0,0 +1,88 @@
+const accountSettings = [
+  {
+    id: "account",
+    title: "Account",
+    items: [
+      { id: "name", label: "Name", type: "text" },
+      { id: "email", label: "Email", type: "email" },
+      { id: "password", label: "Password", type: "password" },
+    ],
+  },
+  {
+    id: "body",
+    title: "Body Information",
+    items: [
+      { id: "height", label: "Height", type: "number" },
+      { id: "weight", label: "Weight", type: "number" },
+      { id: "targetWeight", label: "Target Weight", type: "number" },
+      { id: "age", label: "Age", type: "number" },
+      { id: "gender", label: "Gender", type: "select" },
+    ],
+  },
+  {
+    id: "dietary",
+    title: "Dietary Preferences",
+    items: [
+      {
+        id: "dietaryRestrictions",
+        label: "Dietary Restrictions",
+        type: "multiselect",
+      },
+      {
+        id: "activeDiet",
+        label: "Active Diet",
+        type: "select",
+      },
+    ],
+  },
+  {
+    id: "activity",
+    title: "Activity Level",
+    items: [{ id: "activityLevel", label: "Activity Level", type: "select" }],
+  },
+  {
+    id: "health",
+    title: "Health Conditions",
+    items: [
+      {
+        id: "healthConditions",
+        label: "Health Conditions",
+        type: "multiselect",
+      },
+    ],
+  },
+];
+const DisplaySettings = () => {
+  return (
+    <section className=" bg-white rounded-b-xl overflow-y-auto flex-1 space-y-6 pt-5">
+      {accountSettings.map((settings, idx) => {
+        return (
+          <div key={idx}>
+            <h2 className="text-2xl font-bold mb-4 pl-2">{settings.title}</h2>
+            <div className="flex flex-col  ">
+              {settings.items.map((item, idx) => {
+                return (
+                  <button
+                    key={idx}
+                    className={`flex py-2 px-4 justify-between ${
+                      idx === settings.items.length - 1
+                        ? "border-y"
+                        : "border-t"
+                    }
+                ${idx % 2 === 0 ? "bg-gray-100" : ""}
+                `}
+                  >
+                    <div>{item.label} </div>
+                    <p>{}</p>
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </section>
+  );
+};
+
+export default DisplaySettings;
--- src/app/(dashboard)/profile/settings/page.tsx
+++ src/app/(dashboard)/profile/settings/page.tsx
@@ -1,30 +1,26 @@
-"use client";
-
-import { motion } from "framer-motion";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
+import DisplaySettings from "./components/display-settings";
 
 const SettingsPage = () => {
+  const getUserData = async () => {};
+
   return (
-    <motion.main
-      initial={{ opacity: 0, x: 20 }}
-      animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: -20 }}
-      className="flex flex-col h-[calc(100dvh-4.5rem)] bg-slate-300"
-    >
-      <section className="p-4 bg-white rounded-b-xl">
+    <main className="flex flex-col h-[calc(100dvh-4.5rem)] bg-white">
+      <section className="p-4 shadow-lg border-b-2 ">
+        <h1 className="text-5xl font-bold mb-4 ">Settings</h1>
+
         <Link
           href="/profile"
-          className="flex items-center gap-2 text-slate-500 hover:text-slate-700"
+          className="flex items-center gap-2 text-slate-500 hover:text-slate-700 "
         >
           <ArrowLeft className="w-5 h-5" />
           Back to Profile
         </Link>
-        <h1 className="text-2xl font-bold mt-4">Settings</h1>
       </section>
 
-      {/* Add your settings content here */}
-    </motion.main>
+      <DisplaySettings />
+    </main>
   );
 };
 
