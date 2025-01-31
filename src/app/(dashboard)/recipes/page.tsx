29f588b546b0693a53b5193c91888670ea92a40e
Yash <yashwanth.venkatesan@gmail.com>
2025-01-30 19:29:23 -0500

    started recipes page input field

--- src/app/(dashboard)/recipes/page.tsx
+++ src/app/(dashboard)/recipes/page.tsx
@@ -1,7 +1,20 @@
+import { Input } from "@/components/ui/input";
 import React from "react";
 
 const RecipePage = () => {
-  return <div>RecipePage</div>;
+  return (
+    <main className="flex-1 px-4 pt-8 space-y-4">
+      <header>
+        <h1 className="text-4xl font-bold">Recipes</h1>
+      </header>
+
+      <Input placeholder="Search for a recipe" />
+
+      <section className="flex flex-col gap-4">
+        <h2 className="text-2xl font-bold">Filters</h2>
+      </section>
+    </main>
+  );
 };
 
 export default RecipePage;
